e0f86e23fdf045beafe79a3692c103ff7a93da65
Jordan P. Day <jday27@mymts.net>
2015-03-20 10:27:15 -0500

    Adding comments to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+
+  #CREATE
   get  'products/new' => 'products#new' #showing the new form
   post 'products' => 'products#create'  #creating the new product
 
+  #SHOW
   get  'products' => 'products#index'
   get  'products/:id' => 'products#show', as: 'product'
 
-  
+
+
 end
