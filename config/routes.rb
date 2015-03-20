7d57d3431d8f9c75f708aad48a3798f7869264b0
Jordan P. Day <jday27@mymts.net>
2015-03-20 10:17:41 -0500

    Updating all files

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get  'products/new' => 'products#new'
-  post 'products' => 'products#create'
+  get  'products/new' => 'products#new' #showing the new form
+  post 'products' => 'products#create'  #creating the new product
 
   get  'products' => 'products#index'
   get  'products/:id' => 'products#show', as: 'product'
