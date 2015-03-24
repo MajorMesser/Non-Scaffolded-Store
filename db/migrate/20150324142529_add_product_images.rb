7307f50663794bc5adba53a62ec3651d701c0c1b
Jordan P. Day <jday27@mymts.net>
2015-03-24 09:29:39 -0500

    adding image path column to products

---
+++ db/migrate/20150324142529_add_product_images.rb
@@ -0,0 +1,4 @@
+class AddProductImages < ActiveRecord::Migration
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150316172118) do
+ActiveRecord::Schema.define(version: 20150324142758) do
 
   create_table "products", force: true do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20150316172118) do
     t.decimal  "price"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image_filename"
   end
 
 end
